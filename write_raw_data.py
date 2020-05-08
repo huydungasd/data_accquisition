e4daa115e6ce85a5adb86c542989d93f770cce4a
huydungasd <huydungasd@gmail.com>
2020-05-08 20:22:48 +0200

    _

--- write_raw_data.py
+++ write_raw_data.py
@@ -20,7 +20,7 @@ def main():
 	board.analog[1].enable_reporting()
 	board.analog[2].enable_reporting()
 
-	bno = BNO055.BNO055(serial_port='/dev/ttyAMA0', rst=18)
+	bno = BNO055.BNO055(serial_port='/dev/ttyAMA0')
 	bno.begin()#mode=BNO055_MODE_ACCONLY)
 
 	com_all_data = create_command(BNO055_ACCEL_DATA_X_LSB_ADDR, 32)
