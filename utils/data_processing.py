ed46f6d81e0740f1d7b2293aca344895b798af79
huydungasd <huydungasd@gmail.com>
2020-05-08 16:59:25 +0200

    _

--- utils/data_processing.py
+++ utils/data_processing.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import scipy.interpolate
 
 
 def data_transform(data, sensibility):
@@ -17,3 +18,14 @@ def irread_to_cm(data):
 	# Source: https://www.upgradeindustries.com/product/58/Sharp-10-80cm-Infrared-Distance-Sensor-(GP2Y0A21YK0F)
     return (data * 5) ** -1.15 * 27.86
 
+def interpolate_3dvector_linear(input, input_timestamp, output_timestamp):
+    assert input.shape[0] == input_timestamp.shape[0]
+    func = scipy.interpolate.interp1d(input_timestamp, input, axis=0)
+    interpolated = func(output_timestamp)
+    return interpolated
+
+def create_imu_data_deep(filepath, frequency=100):
+    df = pd.read_csv(filepath)
+    df_interp = interpolate_3dvector_linear(df, df.iloc[:, 0], np.arange(df.iloc[0, 0], df.iloc[-1, 0], 0.01))
+    df_interp = pd.DataFrame(df_interp, columns=list(df.columns))
+    return df_interp.iloc[:, 0:10]
--- write_raw_data.py
+++ write_raw_data.py
@@ -1,50 +1,54 @@
 import csv
 import numpy as np
+import argparse
 
 from utils.data_acq import *
 from pyfirmata import Arduino, util
 from Adafruit_BNO055 import BNO055
 
 
-com_all_data = create_command(BNO055_ACCEL_DATA_X_LSB_ADDR, 32)
+def main():
+	parser = argparse.ArgumentParser()
+    parser.add_argument('output', type=int, help='Output name')
+    args = parser.parse_args()
 
-board = Arduino('/dev/ttyACM0')
-print('IR Sensor init...')
-it = util.Iterator(board)
-it.start()
-board.analog[0].enable_reporting()
-board.analog[1].enable_reporting()
-board.analog[2].enable_reporting()
+	board = Arduino('/dev/ttyACM0')
+	print('IR Sensor init...')
+	it = util.Iterator(board)
+	it.start()
+	board.analog[0].enable_reporting()
+	board.analog[1].enable_reporting()
+	board.analog[2].enable_reporting()
 
-bno = BNO055.BNO055(serial_port='/dev/ttyAMA0', rst=18)
-bno.begin()#mode=BNO055_MODE_ACCONLY)
+	bno = BNO055.BNO055(serial_port='/dev/ttyAMA0', rst=18)
+	bno.begin()#mode=BNO055_MODE_ACCONLY)
 
-raw_dir = './raw_data/'
-n_files = len([name for name in os.listdir(raw_dir) if os.path.isfile(raw_dir + name)])
-if n_files > 0:
-	if os.stat(f'./raw_data/{n_files - 1}.csv').st_size == 0:
-		n_files -= 1
-f = open(f'./raw_data/{n_files}.csv', 'w', newline='')
-writer = csv.writer(f)
-writer.writerow(['time', 'acc raw', '', '', '', '', '', 'mag raw', '', '',  '', '', '', 'gyr raw', '', '', \
-		'', '', '', 'orientation fusion', '', '', '', '', '',  'quaternion fusion', '', '', '', '', '', \
-		'', '', 'IR on A0', 'IR on A1', 'IR on A2'])
+	com_all_data = create_command(BNO055_ACCEL_DATA_X_LSB_ADDR, 32)
 
-#write_to_register(bno, BNO055_PAGE_ID_ADDR, 0x01, 'Page ID')
-#write_to_register(bno, BNO055_ACC_CONFIG, 0b00011101, 'Acc Config')
-#write_to_register(bno, BNO055_MAG_CONFIG, 0b00000000, 'Mag Config')
-#write_to_register(bno, BNO055_PAGE_ID_ADDR, 0x00, 'Page ID')
+	f = open(f'./raw_data/{args.output}.csv', 'w', newline='')
+	writer = csv.writer(f)
+	writer.writerow(['time', 'acc raw', '', '', '', '', '', 'mag raw', '', '',  '', '', '', 'gyr raw', '', '', \
+			'', '', '', 'orientation fusion', '', '', '', '', '',  'quaternion fusion', '', '', '', '', '', \
+			'', '', 'IR on A0', 'IR on A1', 'IR on A2'])
 
-calibration = False
-while not calibration:
-	sys, gyro, accel, mag = bno.get_calibration_status()
-	print('Sys_cal={0} Gyro_cal={1} Accel_cal={2} Mag_cal={3}'.format(sys, gyro, accel, mag))
-	if sys == 3 & gyro == 3 & accel == 3 & mag == 3:
-		os.system('clear')
-		print('Writing .csv file...')
-		calibration = True
+	#write_to_register(bno, BNO055_PAGE_ID_ADDR, 0x01, 'Page ID')
+	#write_to_register(bno, BNO055_ACC_CONFIG, 0b00011101, 'Acc Config')
+	#write_to_register(bno, BNO055_MAG_CONFIG, 0b00000000, 'Mag Config')
+	#write_to_register(bno, BNO055_PAGE_ID_ADDR, 0x00, 'Page ID')
 
-while True:
-	# Read all data at once and write to scv file
-	all_data = read_raw_data(bno, com_all_data, length=32)
-	writer.writerow([time.time(), *all_data, board.analog[0].read(), board.analog[1].read(), board.analog[2].read()])
+	calibration = False
+	while not calibration:
+		sys, gyro, accel, mag = bno.get_calibration_status()
+		print('Sys_cal={0} Gyro_cal={1} Accel_cal={2} Mag_cal={3}'.format(sys, gyro, accel, mag))
+		if sys == 3 & gyro == 3 & accel == 3 & mag == 3:
+			os.system('clear')
+			print('Writing .csv file...')
+			calibration = True
+
+	while True:
+		# Read all data at once and write to scv file
+		all_data = read_raw_data(bno, com_all_data, length=32)
+		writer.writerow([time.time(), *all_data, board.analog[0].read(), board.analog[1].read(), board.analog[2].read()])
+
+if __name__ == '__main__':
+    main()
